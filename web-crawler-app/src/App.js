e811b7b6379ee49dfb82c5af13cb67861ed360ec
Matt Stoker <matt.mstoker@gmail.com>
2019-04-30 16:11:47 +0100

    adds components for app

--- web-crawler-app/src/App.js
+++ web-crawler-app/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Footer from './components/Footer.jsx';
+import Header from './components/Header.jsx';
+import UrlSearch from './components/UrlSearch.jsx';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <UrlSearch />
+      <Footer />
     </div>
   );
 }
